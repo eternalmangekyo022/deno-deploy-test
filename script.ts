b2fc8ebc56d43bada1128fdb25440ce030d0c107
Tibor Fazekas <tibcso0322@gmail.com>
2023-06-10 18:55:34 +0200

    Update script.ts

--- script.ts
+++ script.ts
@@ -1,11 +1,29 @@
 import { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
 import { oakCors } from "https://deno.land/x/cors/mod.ts";
 
+const books = new Map<string, any>();
+books.set("1", {
+  id: "1",
+  title: "Frankenstein",
+  author: "Mary Shelley",
+});
+
 const router = new Router();
 router
-  .get("/", async () => {
-    await send('hi')
+  .get("/", async (context) => {
+    await send(context, context.request.url.pathname, {
+      root: `${Deno.cwd()}/static`,
+      index: "index.html",
+    });
   })
+  .get("/book", (context) => {
+    context.response.body = Array.from(books.values());
+  })
+  .get("/book/:id", (context) => {
+    if (context.params && context.params.id && books.has(context.params.id)) {
+      context.response.body = books.get(context.params.id);
+    }
+  });
 
 const app = new Application();
 app.use(oakCors()); // Enable CORS for All Routes
