aafb5772fe22d3e1319f377460ee6a8bb1851bb0
Tibor Fazekas <tibcso0322@gmail.com>
2023-06-10 19:02:13 +0200

    Update script.ts

--- script.ts
+++ script.ts
@@ -1,17 +1,10 @@
 import { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
 import { oakCors } from "https://deno.land/x/cors/mod.ts";
 
-const books = new Map<string, any>();
-books.set("1", {
-  id: "1",
-  title: "Frankenstein",
-  author: "Mary Shelley",
-});
-
 const router = new Router();
 router
   .get("/", async (context) => {
-    context.response.body = 'fesfsef'
+    context.response.body = await (await fetch('https://nycpokemap.com/raids.php', { headers: { 'Content-Type': 'application/json' } })).json()
   })
 
 const app = new Application();
