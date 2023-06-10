74fbb49d473868d454916f5d1641914e2ddad045
Tibor Fazekas <tibcso0322@gmail.com>
2023-06-10 18:48:05 +0200

    Update script.ts

--- script.ts
+++ script.ts
@@ -1,5 +1,14 @@
-import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
+import express from 'npm:express'
+import cors from 'npm:cors'
 
-serve(
-    async() => new Response(JSON.stringify(await (await fetch("https://nycpokemap.com/raids.php")).json()))
-)
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+	const data = await (await fetch('https://nycpokemap.com/raids.php')).json()
+	res.json(data)
+})
+
+const PORT = 80;
+app.listen(PORT, () => console.log(PORT))
